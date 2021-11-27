e2bd37ad4a708249d00b1ac463e65eef7b24ee60
OrtwIn <bertil.braun.private@gmail.com>
2021-11-27 11:53:17 +0100

    Updated Email sending

--- Scanner.py
+++ Scanner.py
@@ -6,20 +6,18 @@ URL = "https://sifi.impfomizer.de/selectevent"
 EMAIL = "bertil.braun.business@gmail.com"
 PASSWORD = os.environ.get("password")
 TO_EMAIL = ["uliundoli@gmx.de"]
+MESSAGE = f"Impftermin möglich: {URL}"
 
 
 def send_email():
-    server = smtplib.SMTP("smtp.gmail.com", 465)
-    server.ehlo()
-    server.starttls()
-    server.ehlo()
+    server = smtplib.SMTP_SSL("smtp.gmail.com")
 
     server.login(EMAIL, PASSWORD)
+    server.sendmail(EMAIL, TO_EMAIL, MESSAGE)
 
-    server.sendmail(EMAIL, TO_EMAIL,
-                    "Impftermin möglich: https://sifi.impfomizer.de/selectevent")
-    print("Email sent")
     server.quit()
+
+    print("Email sent")
 
 
 def main():
