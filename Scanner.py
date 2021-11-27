8b9101fe4c1211c2c28721ea78fd5aa331908a41
OrtwIn <bertil.braun.private@gmail.com>
2021-11-27 11:25:49 +0100

    Implemented Scanner for new site in Sifi

--- Scanner.py
+++ Scanner.py
@@ -1,55 +1,41 @@
+import os
 import time
-from util import *
+import requests
+import smtplib
+from dotenv import load_dotenv
+load_dotenv()
 
-# TODO enter the plz of the stations you want to monitor
-plzs = [
-    "70174",
-    "72072",
-    "72213",
-    "71065",
-    "70629"
-]
+URL = "https://sifi.impfomizer.de/selectevent"
+EMAIL = "bertil.braun.business@gmail.com"
+PASSWORD = os.environ.get("password")
+TO_EMAIL = ["uliundoli@gmx.de"]
 
 
-def crawl(plz, driver):
-    url = "https://229-iz.impfterminservice.de/impftermine/service?plz=" + plz
+def send_email():
+    server = smtplib.SMTP("smtp.gmail.com", 465)
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
 
-    driver.get(url)
-    time.sleep(2)
+    server.login(EMAIL, PASSWORD)
 
-    try:
-        driver.find_element_by_xpath("/html/body/section/div[2]/div/div/h1")
-    except:
-        driver.execute_script(
-            "arguments[0].click();",
-            driver.find_element_by_xpath(
-                "/html/body/app-root/div/app-page-its-login/div/div/div[2]/app-its-login-user/div/div/app-corona-vaccination/div[2]/div/div/label[2]/input"
-            ))
-
-        time.sleep(5)
-        try:
-            driver.find_element_by_xpath(
-                "/html/body/app-root/div/app-page-its-login/div/div/div[2]/app-its-login-user/div/div/app-corona-vaccination/div[3]/div/div/div/div[2]/div/app-corona-vaccination-no/div[1]"
-            )
-            onSuccess(url)
-        except:
-            print("element not found")
+    server.sendmail(EMAIL, TO_EMAIL,
+                    "Impftermin möglich: https://sifi.impfomizer.de/selectevent")
+    print("Email sent")
+    server.quit()
 
 
 def main():
-    driver = create()
+    while True:
+        req = requests.get(URL)
 
-    try:
-        for i in range(500):
-            print("Next Iteration:", i)
-            for plz in plzs:
-                crawl(plz, driver)
+        if "Impftermin auswählen" in req.text:
+            print("Impfung möglich")
+            send_email()
+        else:
+            print("Impfung nicht möglich")
 
-            driver.get("https://www.google.com")
-            time.sleep(180)
-
-    except KeyboardInterrupt:
-        destroy(driver)
+        time.sleep(60 * 10)  # sleep 10 minutes
 
 
 if __name__ == "__main__":
